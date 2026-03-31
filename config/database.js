23883c0b1de13448e21bb56915d441a8b88dce9f
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2026-03-31 16:27:48 -0400

    chore: update npm versions

--- config/database.js
+++ config/database.js
@@ -3,15 +3,15 @@ module.exports = ({ env }) => {
 
   const connectionConfig = env('DATABASE_URL')
     ? {
-        connectionString: env('DATABASE_URL'),
-      }
+      connectionString: env('DATABASE_URL'),
+    }
     : {
-        host: env('DATABASE_HOST', '127.0.0.1'),
-        port: env.int('DATABASE_PORT', 5432),
-        database: env('DATABASE_NAME', 'groberplus_db'),
-        user: env('DATABASE_USERNAME', 'postgres'),
-        password: env('DATABASE_PASSWORD', 'postgres'),
-      };
+      host: env('DATABASE_HOST', '127.0.0.1'),
+      port: env.int('DATABASE_PORT', 5432),
+      database: env('DATABASE_NAME', 'eurogrober_db'),
+      user: env('DATABASE_USERNAME', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'postgres'),
+    };
 
   const connections = {
     postgres: {
