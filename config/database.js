a9ce402bb17f72b428d521c28b3ee59fce309803
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2026-03-31 23:50:37 -0400

    chore<config/database>: remove default env values

--- config/database.js
+++ config/database.js
@@ -6,11 +6,11 @@ module.exports = ({ env }) => {
       connectionString: env('DATABASE_URL'),
     }
     : {
-      host: env('DATABASE_HOST', '127.0.0.1'),
-      port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'eurogrober_db'),
-      user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
+      host: env('DATABASE_HOST'),
+      port: env.int('DATABASE_PORT'),
+      database: env('DATABASE_NAME'),
+      user: env('DATABASE_USERNAME'),
+      password: env('DATABASE_PASSWORD'),
     };
 
   const connections = {
